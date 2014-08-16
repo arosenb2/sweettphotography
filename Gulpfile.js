7fd015e9d70e2c449e7a50e18b4fa3da4a7e8bca
Mobility Labs <aaron.rosenbaum@cgifederal.com>
2014-08-15 20:18:00 -0400

    Today's Progress

---
+++ Gulpfile.js
@@ -0,0 +1,80 @@
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    plumber = require('gulp-plumber'),
+    svgSprite = require("gulp-svg-sprites"),
+    rename = require('gulp-rename'),
+    del = require('del');
+
+var EXPRESS_PORT = 4000;
+var EXPRESS_ROOT = __dirname;
+var LIVERELOAD_PORT = 35729;
+
+function startExpress() {
+  var express = require('express');
+  var app = express();
+  app.use(require('connect-livereload')());
+  app.use(express.static(EXPRESS_ROOT));
+  app.listen(EXPRESS_PORT);
+}
+
+// We'll need a reference to the tinylr
+// object to send notifications of file changes
+// further down
+var lr;
+function startLiveReload() {
+
+  lr = require('tiny-lr')();
+  lr.listen(LIVERELOAD_PORT);
+}
+
+function notifyLiveReload(event) {
+  // `gulp.watch()` events provide an absolute path
+  // so we need to make it relative to the server root
+  var fileName = require('path').relative(EXPRESS_ROOT, event.path);
+  console.log(fileName + " was changed.")
+  lr.changed({
+    body: {
+      files: [fileName]
+    }
+  });
+}
+
+gulp.task('default',['express','livereload'],function () {
+  gulp.watch('src/sass/*.scss', ['css']);
+  gulp.watch('src/svg/*.svg', ['svg']);
+  gulp.watch('*.html', notifyLiveReload);
+  gulp.watch('dist/css/*.css', notifyLiveReload);
+});
+
+gulp.task('express',function(){
+  startExpress();
+})
+
+gulp.task('livereload',function(){
+  startLiveReload();
+})
+
+gulp.task('css', function() {
+  return gulp.src('src/sass/global.scss')
+    .pipe(plumber())
+    .pipe(sass({ style: 'expanded' }))
+    .pipe(autoprefixer('last 2 version'))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('svg', function () {
+  return gulp.src('src/svg/*.svg')
+    .pipe(svgSprite({mode: 'defs',svgId:'icon-%f',preview:false}))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/sass/*.scss', ['css']);
+  gulp.watch('*.html', notifyLiveReload);
+  gulp.watch('src/css/*.css', notifyLiveReload);
+});
